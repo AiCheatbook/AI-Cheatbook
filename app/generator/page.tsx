74b542d7d86e8bfea2ff6f3453d1bb0d5492b330
Veer <filesharingid5@gmail.com>
2026-09-04 00:23:06 +0530

    Update community homepage

--- app/generator/page.tsx
+++ app/generator/page.tsx
@@ -2,7 +2,7 @@ import PromptComposer from "@/components/generator/PromptComposer";
 
 export default function GeneratorPage() {
   return (
-    <main className="min-h-screen bg-black px-4 py-16 text-white sm:px-6 lg:px-8">
+    <main className="min-h-screen bg-white px-4 py-16 text-zinc-900 sm:px-6 lg:px-8">
       <PromptComposer />
     </main>
   );
